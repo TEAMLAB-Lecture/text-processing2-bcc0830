ac75d0f1654d1970a6d9284a5ec8a9ebe15f29a8
bcc0830 <bcc0830@naver.com>
2021-01-19 17:58:38 +0900

    2021.01.19

--- text_processing2.py
+++ text_processing2.py
@@ -4,7 +4,7 @@
 
 
 def digits_to_words(input_string):
-    res = {'1' : 'one', '2' : 'two', '3' : 'three', '4' : 'four', '5' : 'five', '6' : 'six', '7' : 'seven',
+    res = {'0' : 'zero' , '1' : 'one', '2' : 'two', '3' : 'three', '4' : 'four', '5' : 'five', '6' : 'six', '7' : 'seven',
            '8' : 'eight', '9' : 'nine'}
     digit_string = []
     for i in input_string:
@@ -23,7 +23,7 @@ def digits_to_words(input_string):
 
 def to_camel_case(underscore_str):
     res = underscore_str.split('_')
-    camelcase_str = None
+    camelcase_str = ''
     for i in range(len(res)):
         if i == 0:
             camelcase_str += res[i]
